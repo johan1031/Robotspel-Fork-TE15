bb1644da376e320f7c700b031486f0f861513917
johan1031 <johan.carrby@gmail.com>
2018-02-20 10:37:57 +0100

    centered playbutton

--- src/robotgame/main.java
+++ src/robotgame/main.java
@@ -5,7 +5,9 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,8 +15,8 @@ public class main extends Application{
 
 	public static Scene scene;
 
-	static int WorldWith = 1300;
-	static int WorldHeigth = 600;
+	static int WorldWidth = 1300;
+	static int WorldHeight = 600;
 
 	private static Group root;
 	private static Group menuGroup = new Group();
@@ -23,6 +25,8 @@ public class main extends Application{
 	private static Scene menuScene;
 
 	private static Rectangle playButton;
+	private static Text play;
+	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -33,8 +37,8 @@ public class main extends Application{
 
 		menu();
 
-		scene = new Scene(root,WorldWith,WorldHeigth);
-		menuScene = new Scene(menuGroup,WorldWith,WorldHeigth);
+		scene = new Scene(root,WorldWidth,WorldHeight);
+		menuScene = new Scene(menuGroup,WorldWidth,WorldHeight);
 
 
 
@@ -65,11 +69,22 @@ public class main extends Application{
 	}
 
 	public static void menu(){
-		playButton = new Rectangle(WorldWith*(1.0/5), WorldHeigth*(1.0/5));
-
-		menuGroup.getChildren().addAll(playButton);
+		playButton = new Rectangle(WorldWidth*(1.0/5), WorldHeight*(1.0/5));
+		playButton.setTranslateX(WorldWidth/2-playButton.getWidth()/2);
+		playButton.setTranslateY(WorldHeight/2-playButton.getHeight()/2);
+		playButton.setFill(Color.rgb(0, 255, 238));
+		
+		play = new Text("Play");
+		play.setFont(new Font("Arial", 50));
+		play.setTranslateX(WorldWidth/2-play.getLayoutBounds().getWidth()/2);
+		play.setTranslateY(WorldHeight/2);
+		
+		menuGroup.getChildren().addAll(playButton, play);
 		
 		playButton.setOnMouseClicked(event ->{
+			pStage.setScene(scene);
+		});
+		play.setOnMouseClicked(event ->{
 			pStage.setScene(scene);
 		});
 	}
